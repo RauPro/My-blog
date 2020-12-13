4e04c00f994349e29211c82a4f7b60487b8023c5
RauPro <00012119@uca.edu.sv>
2020-12-13 16:11:24 -0600

    Added links

--- src/Components/Section/Containers/Profile.js
+++ src/Components/Section/Containers/Profile.js
@@ -11,9 +11,9 @@ function Profile(){
             <h3>BEng Student in Computer Science Engineering</h3>
             <h3>Universidad Centroamericana Jose Simeon Cañas</h3>
             <ul className="contacts">
-                <li><a href="#" className="link"><FaGithub/></a></li>
-                <li><a href="#" className="link"><FaLinkedin/></a></li>
-                <li><a href="#" className="link"><FaEnvelope/></a></li>
+                <li><a href="https://github.com/RauPro/" target="_blank" className="link"><FaGithub/></a></li>
+                <li><a href="https://www.linkedin.com/in/RauPro" target="_blank" className="link"><FaLinkedin/></a></li>
+                <li><a href="mailto:00012119@uca.edu.sv" target="_blank" className="link"><FaEnvelope/></a></li>
             </ul>
         </div>
     );
